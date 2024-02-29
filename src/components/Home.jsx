b787ece4277c10152cc2508fb52fd0c8ec4bf01a
Brendan Sikorjak <brendansikorjak@Brendans-MBP.fios-router.home>
2024-02-28 21:44:22 -0500

    added f1 emoji as favicon

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,11 +1,14 @@
 import { useState } from 'react';
+import '../styles/Home.css';
 
 function Home() {
   const [count, setCount] = useState(0);
 
   return (
     <>
-      <h1>It's efF1ng Racing</h1>
+      <h1>
+        🏁 It's ef<span className="f1">F1</span>ng Racing 🏎️
+      </h1>
       <div className="card">
         <button onClick={() => setCount((count) => count + 1)}>
           count is {count}
