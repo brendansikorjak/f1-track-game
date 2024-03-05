092b1b3fb40491234f44d8e8504fdabd02615131
Brendan Sikorjak <brendansikorjak@Brendans-MBP.fios-router.home>
2024-03-04 22:31:00 -0500

    created austrianGP animation

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -2,17 +2,20 @@ import { useState } from 'react';
 import '../styles/Home.css';
 
 function Home() {
-  const [count, setCount] = useState(0);
+  //   const [count, setCount] = useState(0);
 
   return (
     <>
       <h1>
         🏁 It's ef<span className="f1">F1</span>ng Racing 🏎️
       </h1>
-      <div className="card">
+      {/* <div className="card">
         <button onClick={() => setCount((count) => count + 1)}>
           count is {count}
         </button>
+      </div> */}
+      <div className="racetrack">
+        <h1 className="racecar">🏎️</h1>
       </div>
     </>
   );
