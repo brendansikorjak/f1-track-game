5c49f79688bb6418a7d71a25118ae7d8bc7bf4e8
Brendan Sikorjak <brendansikorjak@Brendans-MBP.fios-router.home>
2024-03-06 02:38:33 -0500

    added secret button to handle animation in h1

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -2,11 +2,24 @@ import { useState, useEffect } from 'react';
 import '../styles/Home.css';
 
 function Home() {
+  const [isAnimating, setIsAnimating] = useState(false);
+
+  const handleButtonClick = () => {
+    setIsAnimating(true);
+    setTimeout(() => {
+      setIsAnimating(false);
+    }, 3000);
+  };
   return (
     <>
       <h1>
-        🏁 It's ef<span className="f1">F1</span>ng Racing
-        <span className="slide-out-blurred-left">🏎️</span>
+        🏁 It's ef
+        <button className="f1" onClick={handleButtonClick}>
+          F1
+        </button>
+        ng Racing{' '}
+        <span className={isAnimating ? 'slide-out-blurred-left' : ''}>🏎️</span>
+        {/* <span className="slide-out-blurred-left">🏎️</span> */}
       </h1>
       <div className="racetrack">
         <h1 className="racecar">🏎️</h1>
