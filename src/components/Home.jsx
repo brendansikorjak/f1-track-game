2bef2a4da1adef99b6614a30b0416b84c9e5efa1
Brendan Sikorjak <brendansikorjak@Brendans-MBP.fios-router.home>
2024-03-05 20:16:42 -0500

    added slide-out-blurred-left animation for racecar in h1

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,19 +1,13 @@
-import { useState } from 'react';
+import { useState, useEffect } from 'react';
 import '../styles/Home.css';
 
 function Home() {
-  //   const [count, setCount] = useState(0);
-
   return (
     <>
       <h1>
-        🏁 It's ef<span className="f1">F1</span>ng Racing 🏎️
+        🏁 It's ef<span className="f1">F1</span>ng Racing
+        <span className="slide-out-blurred-left">🏎️</span>
       </h1>
-      {/* <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-      </div> */}
       <div className="racetrack">
         <h1 className="racecar">🏎️</h1>
       </div>
